c3aa768c4e32008544222443915382cdd7135008
admin <admin@d02d81e8-7d5c-4188-9451-3d52d970cc24>
2015-05-07 05:27:01 +0000

    jhjhjhjhjhjhjhjhhj

--- src/com/ApkInfo/UI/MyButtonPanel.java
+++ src/com/ApkInfo/UI/MyButtonPanel.java
@@ -12,9 +12,9 @@ import com.ApkInfo.UIUtil.Theme;
 public class MyButtonPanel extends JPanel{
 	
 	MyButtonPanel() {
-		this.add(new StandardButton("Manifest 보기",Theme.GRADIENT_LIGHTBLUE_THEME,ButtonType.BUTTON_ROUNDED),BorderLayout.WEST);
-		this.add(new StandardButton("탐색기",Theme.GRADIENT_LIGHTBLUE_THEME,ButtonType.BUTTON_ROUNDED),BorderLayout.WEST);
+		this.add(new StandardButton("Manifest 蹂닿린",Theme.GRADIENT_LIGHTBLUE_THEME,ButtonType.BUTTON_ROUNDED),BorderLayout.WEST);
+		this.add(new StandardButton("�먯깋湲�",Theme.GRADIENT_LIGHTBLUE_THEME,ButtonType.BUTTON_ROUNDED),BorderLayout.WEST);
 		
-		this.add(new StandardButton("설치",Theme.GRADIENT_LIGHTBLUE_THEME,ButtonType.BUTTON_ROUNDED),BorderLayout.EAST);		
+		this.add(new StandardButton("�ㅼ튂",Theme.GRADIENT_LIGHTBLUE_THEME,ButtonType.BUTTON_ROUNDED),BorderLayout.EAST);		
 	}	
 }
