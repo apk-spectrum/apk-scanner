687860c92f59b35da1135db6ba2c5f46f9398ed6
admin <admin@d02d81e8-7d5c-4188-9451-3d52d970cc24>
2015-05-11 07:54:15 +0000

    file size

--- src/com/ApkInfo/TabUI/MyTabUIbasicInfo.java
+++ src/com/ApkInfo/TabUI/MyTabUIbasicInfo.java
@@ -1,6 +1,7 @@
 package com.ApkInfo.TabUI;
 
 import java.awt.GridLayout;
+import java.text.DecimalFormat;
 
 import javax.swing.JComponent;
 import javax.swing.JPanel;
@@ -34,6 +35,7 @@ public class MyTabUIbasicInfo extends JComponent{
         strTabInfo += "Package : " + ApkInfo.strPackageName +"\n";
         strTabInfo += "VersionName : " +ApkInfo.strVersionName +"\n";
         strTabInfo += "VersionCode : " +ApkInfo.strVersionCode +"\n";
+        strTabInfo += "Size : " + getFileLength(ApkInfo.lApkSize) + " (" + ApkInfo.lApkSize +" byte)\n";
                 
         apkinform.setText(strTabInfo+"Package : com.iloen.melon\nPackage : com.iloen.melon\nPackage : com.iloen.melon\nPackage : com.iloen.melon\nPackage : com.iloen.melon");
         
@@ -65,7 +67,38 @@ public class MyTabUIbasicInfo extends JComponent{
         
         this.setLayout(new GridLayout(2, 1));
         
-        
-		
 	}
+
+	public static String getFileLength(long length) {
+		double LengthbyUnit = (double) length;
+		int Unit = 0;
+		while (LengthbyUnit > 1024 && Unit < 5) { // 단위 숫자로 나누고 한번 나눌 때마다 Unit
+			LengthbyUnit = LengthbyUnit / 1024;
+			Unit++;
+		}
+
+		DecimalFormat df = new DecimalFormat("#,##0.00");
+
+		 StringBuilder result = new StringBuilder(df.format(LengthbyUnit).length());
+
+		switch (Unit) {
+		case 0:
+			result.append(df.format(LengthbyUnit)+" Bytes");
+			break;
+		case 1:
+			result.append(df.format(LengthbyUnit)+" KB");
+			break;
+		case 2:
+			result.append(df.format(LengthbyUnit)+" MB");
+			break;
+		case 3:
+			result.append(df.format(LengthbyUnit)+" GB");
+			break;
+		case 4:
+			result.append(df.format(LengthbyUnit)+" TB");
+		}
+
+		return result.toString();
+	}
+
 }
