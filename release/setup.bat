b22a83b2d0c8847de9b8fcfcd9c3f69b3cf04c03
감성규/S/W PL그룹(무선)/S4(선임)/삼성전자 <sunggyu.kam@samsung.com>
2017-03-31 22:06:07 +0900

    windows 셋업 배치 파일 수정
    레지스터 등록 과정 제거 및 불필요한 주석 제거

--- release/setup.bat
+++ release/setup.bat
@@ -3,8 +3,10 @@
 @echo off
 
 cd
+
 set APP_PATH=C:\Program Files\APKScanner
 set APP_FILE=ApkScanner.exe
+
 set SRC_PATH=%~dp0.
 echo SRC_PATH : %SRC_PATH%
 
@@ -18,7 +20,7 @@ if not exist "%SRC_PATH%\%APP_FILE%" (
     )
 )
 
-rem --- Java 버전확인 ---
+rem --- Check java version ---
 java -version > javaver.txt 2>&1
 set /p java_ver=<javaver.txt
 del javaver.txt
@@ -30,67 +32,53 @@ set java_ver=%java_ver:java version =%
 set java_ver=%java_ver:"=%
 set java_ver=%java_ver:~,3%
 
-rem Java 1.7버전 이상필요
+rem --- Need Java 1.7 ---
 if not "%java_ver%" GEQ "1.7" (
     echo Need JDK7...
     echo current version : %java_ver%
     goto nosuch_java
 )
 
+rem --- Kill a running demon of adb ---
+if exist "%APP_PATH%\tool\adb.exe" (
+    echo adb kill-server
+    "%APP_PATH%\tool\adb.exe" kill-server
+)
+
+rem --- Remove an existed folder ---
 rmdir /s /q "%APP_PATH%"
 
-rem --- 폴더 생성 ---
+rem --- Create folders ---
 if not exist "%APP_PATH%" (
     echo Create folder : %APP_PATH%
     mkdir "%APP_PATH%"
-    rem mkdir "%APP_PATH%\tool"
 )
 if not exist "%APP_PATH%\tool" (
      echo Create folder : %APP_PATH%\tool
      mkdir "%APP_PATH%\tool"
-     mkdir "%APP_PATH%\tool\lib"
 )
-if not exist "%APP_PATH%\data" (
-     echo Create folder : %APP_PATH%\data
-     mkdir "%APP_PATH%\data"
+if not exist "%APP_PATH%\tool\lib" (
+     echo Create folder : %APP_PATH%\tool\lib
+     mkdir "%APP_PATH%\tool\lib"
 )
 if not exist "%APP_PATH%\lib" (
      echo Create folder : %APP_PATH%\lib
      mkdir "%APP_PATH%\lib"
 )
 if not exist "%APP_PATH%" (
-    echo Fail : Not create the folder : %APP_PATH%
+    echo Fail : No created a folder : %APP_PATH%
     goto exit
 )
 
-rem --- 파일 복사 ---
+rem --- Copy files ---
 copy /Y "%SRC_PATH%\ApkScanner.exe" "%APP_PATH%"
 copy /Y "%SRC_PATH%\APKInfoDlg.jar" "%APP_PATH%"
 copy /Y "%SRC_PATH%\lib\*" "%APP_PATH%\lib"
-rem copy /Y "%SRC_PATH%\lib\apktool.jar" "%APP_PATH%\lib"
-rem copy /Y "%SRC_PATH%\lib\commons-cli-1.3.1.jar" "%APP_PATH%\lib"
-rem copy /Y "%SRC_PATH%\lib\json-simple-1.1.1.jar" "%APP_PATH%\lib"
 copy /Y "%SRC_PATH%\tool\*" "%APP_PATH%\tool"
 copy /Y "%SRC_PATH%\tool\lib\*" "%APP_PATH%\tool\lib"
-rem copy /Y "%SRC_PATH%\tool\adb.exe" "%APP_PATH%\tool"
-rem copy /Y "%SRC_PATH%\tool\AdbWinApi.dll" "%APP_PATH%\tool"
-rem copy /Y "%SRC_PATH%\tool\AdbWinUsbApi.dll" "%APP_PATH%\tool"
-rem copy /Y "%SRC_PATH%\tool\aapt.exe" "%APP_PATH%\tool"
-rem copy /Y "%SRC_PATH%\data\strings-ko.xml" "%APP_PATH%\data"
-rem copy /Y ".\tool\*" "%APP_PATH%\tool\"
 
-
-rem --- 연결프로그램 지정 ---
-ftype vnd.android.package-archive="%APP_PATH%\%APP_FILE%" "%%1"
-vnd.android.package-archive.reg
-assoc .apk=vnd.android.package-archive
-assoc .ppk=vnd.android.package-archive
-rem ftype vnd.android.package-archive=javaw -jar "-Dfile.encoding=utf-8" "%APP_PATH%\%APP_FILE%" %%1 %%*
-rem reg add "HKCR\vnd.android.package-archive\DefaultIcon" /t REG_SZ /d "%APP_PATH%\%APP_FILE%,1" /f
-
-
-rem attrib -h %USERPROFILE%\AppData\Local\IconCache.db
-rem del %USERPROFILE%\AppData\Local\IconCache.db
+rem --- Launch APK Scanner  ---
+"%APP_PATH%\%APP_FILE%"
 
 echo Complete
 
