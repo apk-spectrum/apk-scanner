84cc238e436f561d7dfa41714f502bc54187963c
Sunggyu Kam <sunggyu.kam@samsung.com>
2017-12-27 17:25:24 +0900

    미사용 권한 정보 추출 스크립트 삭제

--- extract_perm_explanation.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cat strings-ko.xml | sed -r -n ':s;/<string.*name=\"perm.*/{/<\/string>\s*$/!{N;bs;};/product=\"[twn]/d;s/<string.*name=\"([^\"]*)\"[^>]*>/<string name="\1">/p;}' | tee perminfo.xml
-
-_exit 0
