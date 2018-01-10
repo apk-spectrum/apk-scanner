2b5fb139ca0ebc889afce2b31e8ca58fb48eead9
Sunggyu Kam <sunggyu.kam@samsung.com>
2018-01-10 15:22:53 +0900

    ApkScannerVersion 클래스 추가
    Apk스캐너 버전 파싱 및 버전 비교
    
    copy from com.android.ddmlib.AdbVersion

---
+++ src/com/apkscanner/util/ApkScannerVersion.java
@@ -0,0 +1,86 @@
+package com.apkscanner.util;
+
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.android.ddmlib.AdbVersion;
+import com.apkscanner.annotations.NonNull;
+
+public class ApkScannerVersion implements Comparable<AdbVersion> {
+    public static final ApkScannerVersion UNKNOWN = new ApkScannerVersion(-1, -1, -1);
+
+    /** Matches e.g. ".... 1.0.32" */
+    private static final Pattern APKSCANNER_VERSION_PATTERN = Pattern.compile(
+            "^.*(\\d+)\\.(\\d+)\\.(\\d+).*");
+
+    public final int major;
+    public final int minor;
+    public final int micro;
+
+    private ApkScannerVersion(int major, int minor, int micro) {
+        this.major = major;
+        this.minor = minor;
+        this.micro = micro;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(Locale.US, "%1$d.%2$d.%3$d", major, minor, micro);
+    }
+
+    @Override
+    public int compareTo(AdbVersion o) {
+        if (major != o.major) {
+            return major - o.major;
+        }
+
+        if (minor != o.minor) {
+            return minor - o.minor;
+        }
+
+        return micro - o.micro;
+    }
+
+    @NonNull
+    public static ApkScannerVersion parseFrom(@NonNull String input) {
+        Matcher matcher = APKSCANNER_VERSION_PATTERN.matcher(input);
+        if (matcher.matches()) {
+            int major = Integer.parseInt(matcher.group(1));
+            int minor = Integer.parseInt(matcher.group(2));
+            int micro = Integer.parseInt(matcher.group(3));
+            return new ApkScannerVersion(major, minor, micro);
+        } else {
+            return UNKNOWN;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        AdbVersion version = (AdbVersion) o;
+
+        if (major != version.major) {
+            return false;
+        }
+        if (minor != version.minor) {
+            return false;
+        }
+        return micro == version.micro;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = major;
+        result = 31 * result + minor;
+        result = 31 * result + micro;
+        return result;
+    }
+}
