9988a52496cde40db5a2f699e6720a818341471c
Sunggyu Kam <sunggyu.kam@samsung.com>
2019-02-26 13:10:44 +0900

    APK Scanner 2.4 Release 20190226 RC1
    1. Add JADX & Bytecode viewer tools
    2. Add SDK29 information
    3. Link Apk Compare(toolbal & dragdrop)
    4. Add PlugIn(Update checker)
    5. Fix some bugs

--- release/setup.bat
+++ release/setup.bat
@@ -25,9 +25,9 @@ java -version > javaver.txt 2>&1
 set /p java_ver=<javaver.txt
 del javaver.txt
 
-if "%java_ver%" == "%java_ver:java version =%" (
-    goto nosuch_java
-)
+rem if "%java_ver%" == "%java_ver:java version =%" (
+rem     goto nosuch_java
+rem )
 set java_ver=%java_ver:java version =%
 set java_ver=%java_ver:"=%
 set java_ver=%java_ver:~,3%
@@ -36,7 +36,7 @@ rem --- Need Java 1.7 ---
 if not "%java_ver%" GEQ "1.7" (
     echo Need JDK7...
     echo current version : %java_ver%
-    goto nosuch_java
+rem     goto nosuch_java
 )
 
 rem --- Kill a running demon of adb ---
@@ -53,18 +53,6 @@ if not exist "%APP_PATH%" (
     echo Create folder : %APP_PATH%
     mkdir "%APP_PATH%"
 )
-if not exist "%APP_PATH%\tool" (
-     echo Create folder : %APP_PATH%\tool
-     mkdir "%APP_PATH%\tool"
-)
-if not exist "%APP_PATH%\tool\lib" (
-     echo Create folder : %APP_PATH%\tool\lib
-     mkdir "%APP_PATH%\tool\lib"
-)
-if not exist "%APP_PATH%\lib" (
-     echo Create folder : %APP_PATH%\lib
-     mkdir "%APP_PATH%\lib"
-)
 if not exist "%APP_PATH%\data" (
      echo Create folder : %APP_PATH%\data
      mkdir "%APP_PATH%\data"
@@ -73,9 +61,41 @@ if not exist "%APP_PATH%\data\build-master-target-product-security" (
      echo Create folder : %APP_PATH%\data\build-master-target-product-security
      mkdir "%APP_PATH%\data\build-master-target-product-security"
 )
+if not exist "%APP_PATH%\lib" (
+     echo Create folder : %APP_PATH%\lib
+     mkdir "%APP_PATH%\lib"
+)
+if not exist "%APP_PATH%\lib\proxy-vole" (
+     echo Create folder : %APP_PATH%\lib\proxy-vole
+     mkdir "%APP_PATH%\lib\proxy-vole"
+)
 if not exist "%APP_PATH%\plugin" (
      echo Create folder : %APP_PATH%\plugin
      mkdir "%APP_PATH%\plugin"
+)
+if not exist "%APP_PATH%\security" (
+     echo Create folder : %APP_PATH%\security
+     mkdir "%APP_PATH%\security"
+)
+if not exist "%APP_PATH%\tool" (
+     echo Create folder : %APP_PATH%\tool
+     mkdir "%APP_PATH%\tool"
+)
+if not exist "%APP_PATH%\tool\jadx" (
+     echo Create folder : %APP_PATH%\tool\jadx
+     mkdir "%APP_PATH%\tool\jadx"
+)
+if not exist "%APP_PATH%\tool\jadx\bin" (
+     echo Create folder : %APP_PATH%\tool\jadx\bin
+     mkdir "%APP_PATH%\tool\jadx\bin"
+)
+if not exist "%APP_PATH%\tool\jadx\lib" (
+     echo Create folder : %APP_PATH%\tool\jadx\lib
+     mkdir "%APP_PATH%\tool\jadx\lib"
+)
+if not exist "%APP_PATH%\tool\lib" (
+     echo Create folder : %APP_PATH%\tool\lib
+     mkdir "%APP_PATH%\tool\lib"
 )
 if not exist "%APP_PATH%" (
     echo Fail : No created a folder : %APP_PATH%
@@ -85,10 +105,16 @@ if not exist "%APP_PATH%" (
 rem --- Copy files ---
 copy /Y "%SRC_PATH%\ApkScanner.exe" "%APP_PATH%"
 copy /Y "%SRC_PATH%\ApkScanner.jar" "%APP_PATH%"
-copy /Y "%SRC_PATH%\lib\*" "%APP_PATH%\lib"
-copy /Y "%SRC_PATH%\tool\*" "%APP_PATH%\tool"
-copy /Y "%SRC_PATH%\tool\lib\*" "%APP_PATH%\tool\lib"
 copy /Y "%SRC_PATH%\data\build-master-target-product-security\*" "%APP_PATH%\data\build-master-target-product-security"
+copy /Y "%SRC_PATH%\lib\*" "%APP_PATH%\lib"
+copy /Y "%SRC_PATH%\plugin\*" "%APP_PATH%\plugin"
+copy /Y "%SRC_PATH%\security\*" "%APP_PATH%\security"
+copy /Y "%SRC_PATH%\tool\*" "%APP_PATH%\tool"
+copy /Y "%SRC_PATH%\tool\jadx\*" "%APP_PATH%\tool\jadx"
+copy /Y "%SRC_PATH%\tool\jadx\bin\*" "%APP_PATH%\tool\jadx\bin"
+copy /Y "%SRC_PATH%\tool\jadx\lib\*" "%APP_PATH%\tool\jadx\lib"
+copy /Y "%SRC_PATH%\tool\lib\*" "%APP_PATH%\tool\lib"
+
 
 rem --- Launch APK Scanner  ---
 "%APP_PATH%\%APP_FILE%"
--- src/com/apkscanner/resource/Resource.java
+++ src/com/apkscanner/resource/Resource.java
@@ -31,7 +31,7 @@ import com.apkscanner.util.ZipFileUtil;
 public enum Resource
 {
 	STR_APP_NAME				(Type.TEXT, "@app_name"),
-	STR_APP_VERSION				(Type.TEXT, "2.3.5"),
+	STR_APP_VERSION				(Type.TEXT, "2.4"),
 	STR_APP_BUILD_MODE			(Type.TEXT, "eng"),
 	STR_APP_MAKER				(Type.TEXT, "jin_h.lee / sunggyu.kam"),
 	STR_APP_MAKER_EMAIL			(Type.TEXT, "jin_h.lee@samsung.com;sunggyu.kam@samsung.com"),
